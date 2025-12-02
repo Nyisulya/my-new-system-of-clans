c5a8b1b5af43480b32abf9feeb0db40ab8ba50dc
Helenmussa <helenmussa825@gmail.com>
2025-12-02 13:36:41 +0300

    Load AdminLTE from CDN to fix styling

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -331,6 +331,10 @@
 @stop
 
 @section('css')
+    {{-- Load AdminLTE from CDN as fallback --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
     {{-- Mobile Responsive Styles --}}
     <link rel="stylesheet" href="{{ asset('css/mobile-responsive.css') }}">
     
